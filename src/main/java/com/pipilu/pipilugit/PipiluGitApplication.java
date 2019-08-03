e7a81f046553cc6e9b24d9fd027d8ca87b827c06
pipilu <homeswallow@163.com>
2019-08-03 22:24:45 +0800

    create git

---
+++ src/main/java/com/pipilu/pipilugit/PipiluGitApplication.java
@@ -0,0 +1,13 @@
+package com.pipilu.pipilugit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipiluGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipiluGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipilu/pipilugit/PipiluGitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pipilu.pipilugit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PipiluGitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
