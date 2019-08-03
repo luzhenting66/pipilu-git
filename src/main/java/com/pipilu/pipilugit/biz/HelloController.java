35306f940584ba63b4f6a289c06420deb384bc95
pipilu <homeswallow@163.com>
2019-08-03 23:33:40 +0800

    新代码

---
+++ src/main/java/com/pipilu/pipilugit/biz/HelloController.java
@@ -0,0 +1,14 @@
+package com.pipilu.pipilugit.biz;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/{name}")
+    public String name(@PathVariable("name")String name){
+        return "hello,welcome to git ...新建的分支：v.0.1.1"+name;
+    }
+}
