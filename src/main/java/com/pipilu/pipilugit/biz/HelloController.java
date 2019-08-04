b833562fed8417a8317a62742e2f639349d90df1
pipilu <homeswallow@163.com>
2019-08-04 09:36:37 +0800

    v0.1.1修复

--- src/main/java/com/pipilu/pipilugit/biz/HelloController.java
+++ src/main/java/com/pipilu/pipilugit/biz/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @RequestMapping("/{name}")
     public String name(@PathVariable("name")String name){
 
-        return "hello,welcome to git ...新建的分支：v.0.1.2"+name
+        return "hello,welcome to git ...新建的分支：v.0.1.1"+name
                 +" 新加功能";
     }
 }
